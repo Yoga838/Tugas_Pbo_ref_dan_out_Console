f62723ab86f269e82d4cc47ea875f53830e8e908
Yoga <Spartancoc04@gmail.com>
2022-05-14 10:05:09 +0700

    Add project files.

---
+++ asasa/Tugas Ref dan out.cs
@@ -0,0 +1,146 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace TugasPbo
+{
+    class getting_data
+    {
+        private static NpgsqlConnection koneksi()
+        {
+            return new NpgsqlConnection(@"server=localhost;port=5432;user id=postgres;password=Bagus383`;database=kasir;");
+        }
+        public bool ExecuteQuery(out bool info)
+
+        {
+            info = true;
+            try
+            {
+
+                NpgsqlConnection con = koneksi();
+                con.Open();
+                string sql = "select * from pegawai";
+                NpgsqlCommand cmd = new NpgsqlCommand(sql, con);
+                NpgsqlDataAdapter da = new NpgsqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                return info;
+
+            }
+            catch (Exception)
+            {
+                info = false;
+                return info;
+            }
+
+        }
+    }
+    class operasi
+    {
+        private static NpgsqlConnection koneksi()
+        {
+            return new NpgsqlConnection(@"server=localhost;port=5432;user id=postgres;password=Bagus383`;database=kasir;");
+        }
+        public bool insert(ref bool info)
+        {
+            try
+            {
+                NpgsqlConnection con = koneksi();
+                con.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("insert into pegawai(id_pegawai,nama,no_telepon) values('8','angga','990')", con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+                info = true;
+                return info;
+            }
+            catch (Exception)
+            {
+                return info;
+            }
+
+        }
+
+        public bool update(ref bool info)
+        {
+            try
+            {
+                NpgsqlConnection con = koneksi();
+                con.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("update pegawai set nama = yoga, no_telepon = 0876 where id_pegawai = 8", con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+                info = true;
+                return info;
+            }
+            catch (Exception)
+            {
+                return info;
+            }
+
+        }
+        public bool Delete(ref bool info)
+        {
+            try
+            {
+                NpgsqlConnection con = koneksi();
+                con.Open();
+                NpgsqlCommand cmd = new NpgsqlCommand("delete from pegawai where id_pegawai = 8", con);
+                cmd.ExecuteNonQuery();
+                cmd.Dispose();
+                con.Close();
+                info = true;
+                return info;
+            }
+            catch (Exception)
+            {
+                return info;
+            }
+
+        }
+    }
+
+    class program_utama
+    {
+
+        static void Main(string[] args)
+        {
+            bool info;
+            bool ingfo = false;
+            getting_data dat = new getting_data();
+            operasi op = new operasi();
+            if (dat.ExecuteQuery(out info) == true)
+            {
+                Console.WriteLine("sukses mengambil data");
+            }
+            else if (dat.ExecuteQuery(out info) == false)
+            {
+                Console.WriteLine("gagal mengambil data");
+            }
+            if (op.insert(ref ingfo) == true)
+            {
+                Console.WriteLine("insert berhasil");
+            }
+            else if (op.insert(ref ingfo) == false)
+            {
+                Console.WriteLine("insert gagal");
+            }
+            if (op.update(ref ingfo) == true)
+            {
+                Console.WriteLine("update berhasil");
+            }
+            else if (op.update(ref ingfo) == false)
+            {
+                Console.WriteLine("update gagal");
+            }
+            if (op.Delete(ref ingfo) == true)
+            {
+                Console.WriteLine("delete berhasil");
+            }
+            else if (op.Delete(ref ingfo) == false)
+            {
+                Console.WriteLine("delete gagal");
+            }
+        }
+    }
+}
